8f1fa8c29f075f07440ed1a1c78c5ac5671411d7
zabehn <zachary.behn@my.tccd.edu>
2020-01-22 19:35:31 -0600

    project set up

---
+++ labs/PizzaCreator/PizzaCreator/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * ITSE 1430
+ * Zachary Behn
+ */
+
+using System;
+
+namespace PizzaCreator
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
