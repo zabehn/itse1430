d90a63f74a39ff89fd7224e5662aca160eb2f56e
zabehn <zachary.behn@my.tccd.edu>
2020-01-22 21:24:02 -0600

    classwork 1/22

--- classwork/Section1/Section1/Program.cs
+++ classwork/Section1/Section1/Program.cs
@@ -10,12 +10,67 @@ namespace Section1
     {
         static void Main ( string[] args )
         {
+            //PlayingWithVariables();
+            AddMovie();
+            
+        }
+
+        static void AddMovie ()
+        {
+            string title = ReadString("Enter a title: ", true);
+
+            int releaseYear = ReadInt32("Enter the release year (>=0): ", 0, 2100);
+
+            int runLength = ReadInt32("Enter the run length (>=0): ", 0, 86400);
+
+            string description = ReadString("Enter a description: " , false);
+
+            Boolean isClassic = ReadBoolean("Is this a classic movie?");
+        }
+
+        static string ReadString (string message, Boolean isRequired)
+        {
+            Console.Write(message);
+            string value = Console.ReadLine();
+
+            //TODO: validate
+            return value;
+        }
+
+        static bool ReadBoolean ( string message )
+        {
+            Console.Write(message + " (Y/N)");
+            string value = Console.ReadLine();
+
+            //TODO: Do correctly
+            char firstChar = value[0];
+            return firstChar == 'Y';
+        }
+
+        static int ReadInt32 (string message, int minValue, int maxValue)
+        {
+            Console.Write(message);
+            string temp = Console.ReadLine();
+            int value;
+            if (Int32.TryParse(temp, out value))
+                return value;
+            
+            //TODO: validate input
+            return -1;
+        }
+
+        private static void PlayingWithVariables ()
+        {
             Console.WriteLine("Hello World!");
 
             int hours = 5;
             double payRate;
             string name = "maaaaaaaaaaaaaaaaaaan";
             bool pass;
+
+            //"Display Message"
+            Console.WriteLine("Enter a value");
+            string input = Console.ReadLine();
         }
     }
 }
--- labs/PizzaCreator/PizzaCreator/Program.cs
+++ labs/PizzaCreator/PizzaCreator/Program.cs
@@ -15,6 +15,17 @@ namespace PizzaCreator
             Console.WriteLine("ITSE 1430\n" +
                 "Spring 2020\n" +
                 "Zachary Behn\n");
+            printMenu();
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+        }
+
+        static void printMenu()
+        {
+            Console.Write("1. Create Order\n" +
+                "2. View Order\n" +
+                "3. Order Food\n" +
+                "\n Please Enter a value 1 through 3__");
         }
     }
 }
